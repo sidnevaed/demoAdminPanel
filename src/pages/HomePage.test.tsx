505baa6f8d10ffb2e1c3a33b3c87960a0b9e8786
sidneva.e.d <sidneva.e.d@gmail.com>
2023-10-25 14:11:33 +0300

    Final

--- src/pages/HomePage.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import { HomePage } from "./HomePage";
-
-test("HomePage renders", () => {
-  render(<HomePage />);
-  const pageHeading = screen.getByRole("heading", { level: 1 });
-  expect(pageHeading).toBeInTheDocument();
-});
